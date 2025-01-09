fcb347f16b413463252599ece2042dca93e33152
sangil94s <bsc7417@gmail.com>
2025-01-09 20:28:57 +0900

    [Add] 삭제 테스트 추가 중

--- components/Remove.tsx
+++ components/Remove.tsx
@@ -4,20 +4,18 @@
 import axios from "axios";
 
 const DeletePostComponent = () => {
+
 	const handleDelete = async () => {
 	  try {
 		const response = await axios.delete('https://jsonplaceholder.typicode.com/posts/1');
 		console.log('삭제 성공:', response.data);
-		alert('포스트가 성공적으로 삭제되었습니다.');
 	  } catch (error) {
 		console.error('삭제 실패:', error);
-		alert('포스트 삭제에 실패했습니다.');
 	  }
 	};
   
 	return (
 	  <div className="flex flex-col justify-center items-center">
-		<h1>포스트 삭제</h1>
 		<button onClick={handleDelete}>삭제하기</button>
 	  </div>
 	);
---
+++ tests/removeComponent.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('글 삭제 컴포넌트 테스트', () => {
+  test('삭제 컴포넌트를 테스트 하는 목적 입니다다', async ({ page }) => {
+
+  });
+});
